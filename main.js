256bd58585daaaed489f59f2ec833b9cedba93f3
prohorov01 <alexprohorov01@gmail.com>
2023-06-20 13:04:36 +0300

    update

---
+++ main.js
@@ -0,0 +1,15 @@
+let popupWindow;
+
+function openWindow() {
+  popupWindow = window.open(
+    "https://www.google.com",
+    "_blank",
+    "width=800,height=600"
+  );
+}
+
+function closeWindow() {
+  if (popupWindow) {
+    popupWindow.close();
+  }
+}
